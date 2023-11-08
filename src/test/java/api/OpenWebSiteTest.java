606168de7ce1ea69c8cacd580c0f5d84084e01ce
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-11-08 13:14:59 +0300

    refactor code

--- src/test/java/api/OpenWebSiteTest.java
+++ src/test/java/api/OpenWebSiteTest.java
@@ -1,16 +1,13 @@
 package api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 
 public class OpenWebSiteTest {
-    private static final Logger logger = LoggerFactory.getLogger(OpenWebSiteTest.class);
-    @Test
+     @Test
     public void checkOpeningWebSite() {
-        logger.info("Page opens");
+
         given().header("User-Agent", "PostmanRuntime/7.33.0").header("Content-Type", "text/html")
                 .when().get("https://www.mobile.de/")
                 .then().log().status()
--- src/test/java/api/SelectCarTest.java
+++ src/test/java/api/SelectCarTest.java
@@ -1,14 +1,11 @@
 package api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class SelectCarTest {
-    public static Logger logger = LoggerFactory.getLogger(SelectCarTest.class);
-    @Test
+       @Test
     public void checkSelectingCarById() {
                 String requestBode = "{\n" +
                 "\"adId\": \"367152267\",\n" +
@@ -23,6 +20,5 @@ public class SelectCarTest {
                 .when().post("https://suchen.mobile.de/fahrzeuge/svc/my/parkings/367152267")
                 .then().statusCode(201)
                 .assertThat().body("adId", equalTo(carId));
-        logger.info("hello");
-        }
+       }
 }
